ea21d96b245910c31f524da5a4c03b2ba59f4a4b
Geeth <Geethmadhawa99@gmail.com>
2022-09-10 23:00:32 +0530

    color theme added

--- client/src/components/RightSide/RightSide.jsx
+++ client/src/components/RightSide/RightSide.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import "./RightSide.css";
-import Home from "../../img/home.png";
-import Noti from "../../img/noti.png";
-import Comment from "../../img/comment.png";
+
+import { UilHome } from "@iconscout/react-unicons";
 import { UilSetting } from "@iconscout/react-unicons";
+import { UilHistory } from "@iconscout/react-unicons";
+import { UilMessage } from "@iconscout/react-unicons";
 import TrendCard from "../TrendCard/TrendCard";
 import ShareModel from "../ShareModel/ShareModel";
 import { Link } from "react-router-dom";
@@ -15,11 +16,19 @@ const RightSide = () => {
       <div className="navIcons">
         <Link to="../home">
           {" "}
-          <img src={Home} alt="" />
+          <div className="s-icon">
+            <UilHome />
+          </div>
         </Link>
-        <UilSetting />
-        <img src={Noti} alt="" />
-        <img src={Comment} alt="" />
+        <div className="s-icon">
+          <UilSetting />
+        </div>
+        <div className="s-icon">
+          <UilHistory />
+        </div>
+        <div className="s-icon">
+          <UilMessage />
+        </div>
       </div>
       <TrendCard />
       <button className="button r-button" onClick={() => setModelOpened(true)}>
